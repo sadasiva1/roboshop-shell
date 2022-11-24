f7d56ea03548e4448c5e7a54d779840c171928a1
sadasiva1 <113237411+sadasiva1@users.noreply.github.com>
2022-11-24 20:14:32 +0530

    User Script

--- user.sh
+++ user.sh
@@ -12,9 +12,7 @@ cd /home/roboshop/user
 npm install
 
 sed -i -e 's/REDIS_ENDPOINT/redis.sadasiva.online/' -e 's/MONGO_ENDPOINT/mongo.sadasiva.online/'
-curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
-cd /home/roboshop
-unzip /tmp/user.zip
-mv user-main user
-cd /home/roboshop/user
-npm install
+mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl start user
+systemctl enable user
