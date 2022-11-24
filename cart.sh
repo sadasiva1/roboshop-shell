597b2b45fb91ebf1017f3b40ad5dcbcbeac4a182
sadasiva1 <113237411+sadasiva1@users.noreply.github.com>
2022-11-24 20:33:48 +0530

    Cart Script

--- cart.sh
+++ cart.sh
@@ -10,8 +10,10 @@ mv cart-main cart
 cd cart
 npm install
 
-# mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
-# systemctl daemon-reload
-# systemctl start cart
-# systemctl enable cart
+sed -i -e 's/REDIS_ENDPOINT/redis.sadasiva.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.sadasiva.online/'
+
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl restart cart
+systemctl enable cart
 
