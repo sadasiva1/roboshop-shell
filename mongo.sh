fea7a5a346dc977c34d9f210d17ac06e32694ef2
sadasiva1 <113237411+sadasiva1@users.noreply.github.com>
2022-11-24 18:53:26 +0530

    Mongo Script

--- mongo.sh
+++ mongo.sh
@@ -3,5 +3,6 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/robos
 yum install -y mongodb-org
 sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongo.conf
 
+
 systemctl enable mongod
 systemctl restart mongod
