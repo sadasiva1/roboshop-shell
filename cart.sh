68c0edcb0aaf69a606852acba0d5b8a621484255
sadasiva1 <113237411+sadasiva1@users.noreply.github.com>
2022-11-29 08:12:31 +0530

    Mysql Script

--- cart.sh
+++ cart.sh
@@ -1,54 +1,51 @@
+COMPONENT=cart
 source common.sh
 
 PRINT "Install NodeJs Repos"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG
 STAT $?
 
 PRINT "Install NodeJs"
-yum install nodejs -y
+yum install nodejs -y &>>$LOG
 STAT $?
 
 PRINT "Adding Apllication User"
-useradd roboshop
+useradd roboshop &>>$LOG
 STAT $?
 
 PRINT "Download App Content"
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>$LOG
 STAT $?
 
 PRINT "Remove Previous Version App"
 cd /home/roboshop
-rm -rf cart
+rm -rf cart &>>$LOG
 STAT $?
 
 PRINT "Extracting App Content"
-unzip -o /tmp/cart.zip
+unzip -o /tmp/cart.zip &>>$LOG
 STAT $?
 
 mv cart-main cart
 cd cart
 
 PRINT "Install NodeJs Dependencies App"
-npm install
+npm install &>>$LOG
 STAT $?
 
 PRINT "Configure Endpoints For SystemD Configuration"
-sed -i -e 's/REDIS_ENDPOINT/redis.sadasiva.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.sadasiva.online/'
-STAT $?
-
-PRINT "Setup SystemD Service"
-mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+sed -i -e 's/REDIS_ENDPOINT/redis.sadasiva.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.sadasiva.online/' &>>$LOG
 STAT $?
 
 PRINT "Reload SystemD"
-systemctl daemon-reload
+systemctl daemon-reload &>>$LOG
 STAT $?
 
 PRINT "Restart Cart"
-systemctl restart cart
+systemctl restart cart &>>$LOG
 STAT $?
 
 PRINT "Enable Cart Service"
-systemctl enable cart
+systemctl enable cart &>>$LOG
 STAT $?
 
