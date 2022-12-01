cdb043b4f02e753648926f8a80d6f2c6277efcc5
sadasiva1 <113237411+sadasiva1@users.noreply.github.com>
2022-12-01 09:30:56 +0530

    Cart Script

--- common.sh
+++ common.sh
@@ -1,15 +1,15 @@
 STAT() {
-if [ $1 -eq 0 ]; then
+ if [ $1 -eq 0 ]; then
    echo -e "\e[32mSUCCESS\e[0m"
-else
+ else
   echo -e "\e[31mFAILURE\e[0m"
   echo check the error in $LOG file
   exit
-fi
+ fi
 }
 
 PRINT() {
-  echo "--------------$1-------------" >>${LOG}
+  echo "-------------- $1 -------------" >>${LOG}
   echo -e "\e[33m$1\e[0m"
 }
 
