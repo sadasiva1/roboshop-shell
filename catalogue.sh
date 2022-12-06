b3af5542eee9d4f8ea94ab775d0782194e9e464d
sadasiva1 <113237411+sadasiva1@users.noreply.github.com>
2022-12-06 19:19:26 +0530

    Cart Script

--- catalogue.sh
+++ catalogue.sh
@@ -1,4 +1,18 @@
-COMPONENT=catalogue
-source common.sh
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
 
-NODEJS
+useradd roboshop
+
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+cd /home/roboshop
+unzip -o /tmp/catalogue.zip
+mv catalogue-main catalogue
+cd /home/roboshop/catalogue
+npm install
+
+sed -i -e 's/MONGO_DNSNAME/mongodb.sadasiva.online/' systemd.service
+
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl start catalogue
+systemctl enable catalogue
