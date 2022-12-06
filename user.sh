d3744a4ac1c9ef294a7f5e89124866fb84166fc8
sadasiva1 <113237411+sadasiva1@users.noreply.github.com>
2022-12-06 19:28:22 +0530

    Cart Script

--- user.sh
+++ user.sh
@@ -1,4 +1,16 @@
-COMPONENT=user
-source common.sh
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
 
-NODEJS
+curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
+cd /home/roboshop
+unzip /tmp/user.zip
+mv user-main user
+cd /home/roboshop/user
+npm install
+
+sed -i -e 's/REDIS_ENDPOINT/redis.sadasiva.online/' -e 's/MONGO_ENDPOINT/mongo.sadasiva.online/' systemd.servive
+mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl start user
+systemctl enable user
