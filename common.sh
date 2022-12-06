8ae2883b71458ee1c9b0d18b2ae75d32e835116a
sadasiva1 <113237411+sadasiva1@users.noreply.github.com>
2022-12-06 18:46:20 +0530

    Cart Script

--- common.sh
+++ common.sh
@@ -52,6 +52,7 @@ NODEJS() {
   npm install &>>$LOG
   STAT $?
 
+SYSTEMD_SETUP() {
   PRINT "Configure Endpoints For SystemD Configuration"
   sed -i -e 's/REDIS_ENDPOINT/redis.sadasiva.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.sadasiva.online/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG
   mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
