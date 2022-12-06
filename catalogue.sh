87baca7dd8866ab24bbb6e012bc2d932c8dfc8b5
sadasiva1 <113237411+sadasiva1@users.noreply.github.com>
2022-12-06 19:54:42 +0530

    Cart Script

--- catalogue.sh
+++ catalogue.sh
@@ -1,11 +1,4 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
+COMPONENT=catalogue
+source common.sh
 
-useradd roboshop
-
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-cd /home/roboshop
-unzip -o /tmp/catalogue.zip
-mv catalogue-main catalogue
-cd /home/roboshop/catalogue
-npm install
+NODEJS
--- user.sh
+++ user.sh
@@ -1,11 +1,4 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
+COMPONENT=user
+source common.sh
 
-curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
-cd /home/roboshop
-unzip -o /tmp/user.zip
-mv user-main user
-cd /home/roboshop/user
-npm install
-
+NODEJS
