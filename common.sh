d24b0ebdfc4bfda215495b89c8f315d36ce3b100
sadasiva1 <113237411+sadasiva1@users.noreply.github.com>
2022-12-06 18:27:39 +0530

    Cart Script

--- common.sh
+++ common.sh
@@ -15,3 +15,57 @@ PRINT() {
 
 LOG=/tmp/$COMPONENT.log
 rm -f $LOG
+
+NODEJS() {
+  PRINT "Install NodeJs Repos"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG
+  STAT $?
+
+  PRINT "Install NodeJs"
+  yum install nodejs -y &>>$LOG
+  STAT $?
+
+  PRINT "Adding Apllication User"
+  id roboshop &>>$LOG
+  if [ $? -ne 0 ]; then
+   useradd roboshop &>>$LOG
+  fi
+  STAT $?
+
+  PRINT "Download App Content"
+  curl -s -L -o /tmp/${COMPONET}.zip "https://github.com/roboshop-devops-project/${COMPONET}/archive/main.zip" &>>$LOG
+  STAT $?
+
+  PRINT "Remove Previous Version App"
+  cd /home/roboshop &>>$LOG
+  rm -rf ${COMPONET} &>>$LOG
+  STAT $?
+
+  PRINT "Extracting App Content"
+  unzip -o /tmp/${COMPONET}.zip &>>$LOG
+  STAT $?
+
+  mv ${COMPONET}-main ${COMPONET}
+  cd ${COMPONET}
+
+  PRINT "Install NodeJs Dependencies App"
+  npm install &>>$LOG
+  STAT $?
+
+  PRINT "Configure Endpoints For SystemD Configuration"
+  sed -i -e 's/REDIS_ENDPOINT/redis.sadasiva.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.sadasiva.online/' /home/roboshop/${COMPONET}/systemd.service &>>$LOG
+  STAT $?
+
+  PRINT "Reload SystemD"
+  systemctl daemon-reload &>>$LOG
+  STAT $?
+
+  PRINT "Restart ${COMPONET}"
+  systemctl restart ${COMPONET} &>>$LOG
+  STAT $?
+
+  PRINT "Enable ${COMPONET} Service"
+  systemctl enable ${COMPONET} &>>$LOG
+  STAT $?
+
+}
--- user.sh
+++ user.sh
@@ -1,18 +1,4 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
+COMPONENT=user
+source common.sh
 
-
-curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
-cd /home/roboshop
-rm -rf user
-unzip -o /tmp/user.zip
-mv user-main user
-cd /home/roboshop/user
-npm install
-
-sed -i -e 's/REDIS_ENDPOINT/redis.sadasiva.online/' -e 's/MONGO_ENDPOINT/mongo.sadasiva.online/' systemd.service
-mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
-systemctl daemon-reload
-systemctl start user
-systemctl enable user
+NODEJS
