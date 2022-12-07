e5eddb6c9399d4fe35ffba4dc5d8610083af9f2a
sadasiva1 <113237411+sadasiva1@users.noreply.github.com>
2022-12-07 18:13:36 +0530

    Cart Script

--- mysql.sh
+++ mysql.sh
@@ -35,9 +35,20 @@ then
  cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" &>>$LOG
 fi
 
-echo "uninstall plugin validate password"
+PRINT "uninstall plugin validate password"
 echo "show plugins" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} | grep validate_password &>>$LOG
 if [ $? -eq 0 ]; then
   echo "uninstall plugin validate_password;" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG
  fi
  STAT $?
+
+
+APP_LOC=/tmp
+CONTENT=mysql-main
+DOWNLOAD_APP_CODE
+
+cd mysql-main &>>$LOG
+
+PRINT "Load Shipping Schema"
+mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} <shipping.sql &>>$LOG
+STAT $?
