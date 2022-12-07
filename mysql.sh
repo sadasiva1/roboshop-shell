cf92178232d808cf40f1a1d131c4997368c68406
sadasiva1 <113237411+sadasiva1@users.noreply.github.com>
2022-12-07 12:09:54 +0530

    MySql Script

--- mysql.sh
+++ mysql.sh
@@ -4,6 +4,7 @@ if [ $? -eq 0 ]; then
  echo SUCCESS
 else
  echo FAILURE
+ exit
 fi
 
 echo Disable MYSQL 8 Version Repo
@@ -12,6 +13,7 @@ if [ $? -eq 0 ]; then
  echo SUCCESS
 else
  echo FAILURE
+ exit
 fi
 
 echo Install MYSQL
@@ -20,6 +22,7 @@ if [ $? -eq 0 ]; then
  echo SUCCESS
 else
  echo FAILURE
+ exit
 fi
 
 echo Enable MYSQL Service
@@ -28,6 +31,7 @@ if [ $? -eq 0 ]; then
  echo SUCCESS
 else
  echo FAILURE
+ exit
 fi
 
 echo Start MYSQL Service
@@ -36,12 +40,13 @@ if [ $? -eq 0 ]; then
  echo SUCCESS
 else
  echo FAILURE
+ exit
 fi
 
-echo show databases | mysql -uroot -pRoboShop@1
+echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}
 if [ $? -ne 0 ]
 then
- echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" > /tmp/root-pass-sql
+ echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROBOSHOP_MYSQL_PASSWORD}';" > /tmp/root-pass-sql
  DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
  cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
 
