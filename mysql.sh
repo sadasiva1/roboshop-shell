85598a26ed203a5cb46d877eed780fcf6e07095b
sadasiva1 <113237411+sadasiva1@users.noreply.github.com>
2022-12-07 12:36:25 +0530

    MySql Script

--- mysql.sh
+++ mysql.sh
@@ -4,50 +4,33 @@ if [ -z "$1" ]; then
 fi
 ROBOSHOP_MYSQL_PASSWORD=$1
 
+STAT() {
+  if [ $1 -eq 0 ]; then
+   echo SUCCESS
+  else
+   echo FAILURE
+   exit
+  fi
+
 echo -e "\e[33mDownloading MYSQL Repo File\e[0m"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-if [ $? -eq 0 ]; then
- echo SUCCESS
-else
- echo FAILURE
- exit
-fi
+STAT $?
 
 echo Disable MYSQL 8 Version Repo
 dnf module disable mysql -y
-if [ $? -eq 0 ]; then
- echo SUCCESS
-else
- echo FAILURE
- exit
-fi
+STAT $?
 
 echo Install MYSQL
 yum install mysql-community-server -y
-if [ $? -eq 0 ]; then
- echo SUCCESS
-else
- echo FAILURE
- exit
-fi
+STAT $?
 
 echo Enable MYSQL Service
 systemctl enable mysqld
-if [ $? -eq 0 ]; then
- echo SUCCESS
-else
- echo FAILURE
- exit
-fi
+STAT $?
 
 echo Start MYSQL Service
 systemctl restart mysqld
-if [ $? -eq 0 ]; then
- echo SUCCESS
-else
- echo FAILURE
- exit
-fi
+STAT $?
 
 echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}
 if [ $? -ne 0 ]
@@ -57,3 +40,4 @@ then
  cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
 
 fi
+}
