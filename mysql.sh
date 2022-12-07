f95c552759f7867e95fc49484ad0d494407ebc80
sadasiva1 <113237411+sadasiva1@users.noreply.github.com>
2022-12-07 17:49:23 +0530

    Cart Script

--- mysql.sh
+++ mysql.sh
@@ -27,10 +27,10 @@ PRINT "Start MYSQL Service"
 systemctl restart mysqld &>>$LOG
 STAT $?
 
-echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}
+echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG
 if [ $? -ne 0 ]
 then
  echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROBOSHOP_MYSQL_PASSWORD}';" > /tmp/root-pass-sql
  DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
- cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
+ cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" &>>$LOG
 fi
