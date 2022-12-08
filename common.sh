a7bd9b28c50ab86567d2d51bffc12cc7d9f369e8
sadasiva1 <113237411+sadasiva1@users.noreply.github.com>
2022-12-08 17:18:27 +0530

    Shipping Script

--- common.sh
+++ common.sh
@@ -128,4 +128,11 @@ PHYTON() {
     pip3 install -r requirements.txt  &>>$LOG
     STAT $?
 
+
+    USER_ID=$(id -u roboshop)
+    GROUP_ID=$(id -g roboshop)
+    sed -i -e "/uid/ c uid = ${USER_ID}" -e "/uid/ c uid = ${GROUP_ID}" ${COMPONENT}.ini
+
+    SYSTEMD_SETUP
+
     }
