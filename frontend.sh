6e952141dd019d5b3ae0503cac66b09de8a9f79e
sadasiva1 <113237411+sadasiva1@users.noreply.github.com>
2022-12-08 19:47:05 +0530

    Shipping Script

--- frontend.sh
+++ frontend.sh
@@ -16,6 +16,10 @@ PRINT "Copy RoboShop Configuration File"
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 STAT $?
 
+PRINT "Update RoboShop Configuration"
+sed -i -e '/catalogue/ s/localhost/dev-catalogue.sadasiva.online/'  -e '/user/ s/localhost/dev-user.sadasiva.online/' -e '/cart/ s/localhost/dev-cart.sadasiva.online/' -e '/shipping/ s/localhost/dev-shipping.sadasiva.online/' -e '/payment/ s/localhost/dev-payment.sadasiva.online/' /etc/nginx/default.d/roboshop.conf
+STAT $?
+
 PRINT "Enable Nginx Service"
 systemctl enable nginx &>>$LOG
 STAT $?
