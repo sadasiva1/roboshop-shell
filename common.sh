e08047b6ec44736a0baecbb1ab010718072842fd
sadasiva1 <113237411+sadasiva1@users.noreply.github.com>
2022-12-08 16:27:37 +0530

    Shipping Script

--- common.sh
+++ common.sh
@@ -43,6 +43,7 @@ DOWNLOAD_APP_CODE() {
   SYSTEMD_SETUP() {
   PRINT "Configure Endpoints For SystemD Configuration"
   sed -i -e 's/REDIS_ENDPOINT/redis.sadasiva.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.sadasiva.online/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG
+  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
   STAT $?
 
   PRINT "Reload SystemD"
